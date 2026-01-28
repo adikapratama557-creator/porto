431ee7dfc81fd86f1efdb8bbf281461829cb36ab
dikapra98 <adikapratama557@gmail.com>
2026-01-28 18:54:49 +0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,220 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/nextjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools9 from "/assets/tools/Corel.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/photoshop.png";
+import Tools12 from "/assets/tools/sql-server.png";
+import Tools13 from "/assets/tools/premiere-pro.png";
+import Tools14 from "/assets/tools/html.png";
+import Tools15 from "/assets/tools/css.png";
+import Tools16 from "/assets/tools/SSMS.png";
+import Tools17 from "/assets/tools/Capcut.png";
+import Tools18 from "/assets/tools/CSV.png";
+import Tools19 from "/assets/tools/mysql.png";
+
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "Next JS",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 9,
+    gambar: Tools9,
+    nama: "CorelDraw",
+    ket: "Design App",
+    dad: "900",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Photoshop",
+    ket: "Design App",
+    dad: "1100",
+  },
+  {
+    id: 12,
+    gambar: Tools12,
+    nama: "SQL Server",
+    ket: "Database",
+    dad: "1200",
+  },
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "Premier Pro",
+    ket: "Video Editing",
+    dad: "1300",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "HTML",
+    ket: "Language",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "CSS",
+    ket: "Language",
+    dad: "1500",
+  },
+  {
+    id: 16,
+    gambar: Tools16,
+    nama: "SSMS",
+    ket: "Reporting",
+    dad: "1600",
+  },
+  {
+    id: 17,
+    gambar: Tools17,
+    nama: "Capcut",
+    ket: "Video Editing",
+    dad: "1700",
+  },
+  {
+    id: 18,
+    gambar: Tools18,
+    nama: "CSV",
+    ket: "Documentation Validation",
+    dad: "1800",
+  },
+  {
+    id: 19,
+    gambar: Tools19,
+    nama: "MySql",
+    ket: "Framework",
+    dad: "1900",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.jpg";
+import Proyek2 from "/assets/proyek/proyek2.jpg";
+import Proyek3 from "/assets/proyek/proyek3.jpg";
+import Proyek4 from "/assets/proyek/proyek4.jpg";
+import Proyek5 from "/assets/proyek/proyek5.jpg";
+import Proyek6 from "/assets/proyek/proyek6.jpg";
+
+export const listProyek = [
+  {
+    id: 1,
+    image: Proyek1,
+    title: "Tenaga Pendukung IT",
+    subtitle: "Kementerian PPN/Bappenas",
+    fullDescription:"Conducting IT support for the entire program of activities, System maintenance, Website development as needed, Graphic design management, Coordination of relations between the government and IT vendors, Advocacy and correspondence",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://www.instagram.com/dikapra98/",
+    dad: "100",
+  },
+  {
+    id: 2,
+    image: Proyek2,
+    title: "IT Support/ IT Operation",
+    subtitle: "Kano Teknologi Utama",
+    fullDescription:"Assist on installation, configuration, and deployment Manufacturing Systems, Perform Computerized System Validation (CSV) documentation activities for deployment project, Perform and assist Internal Functional/integration Testing and UAT of Manufacturing System, Liaise with business users to deliver project needs.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://www.instagram.com/dikapra98/",
+    dad: "200",
+  },
+  {
+    id: 3,
+    image: Proyek3,
+    title: "Quality Assurance / IT Manufacturing Solutions & Services",
+    subtitle: "Mitra Integrasi Informatika",
+    fullDescription:"Assist on installation, configuration, and deployment Manufacturing Systems, Perform Computerized System Validation (CSV) documentation activities for deployment project, Perform and assist Internal Functional/integration Testing and UAT of Manufacturing System, Liaise with business users to deliver project needs, Identifying user needs and managing databases, Ensuring that databases operate efficiently and without errors, Backing up and restoring data to prevent data loss.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://www.instagram.com/dikapra98/",
+    dad: "300",
+  },
+  {
+    id: 4,
+    image: Proyek4,
+    title: "Bussiness Assistant",
+    subtitle: "Kementerian Koperasi dan UKM",
+    fullDescription:"Supported the preparation of business plans, operational budgets (OPEX), and strategic documents for cooperative units, Assisted in the implementation of SIMKOPDES and ensured accurate data entry and reporting compliance, Coordinated cooperative management with local government agencies to align operations with, national standards, Conducted SWOT analysis and risk mapping to strengthen strategic planning across business units, Acted as liaison between cooperative leadership and the Ministry of Cooperatives (Kemenkop), to ensure program alignment and timely reporting.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://www.instagram.com/dikapra98/",
+    dad: "400",
+  },
+   
+  
+];
